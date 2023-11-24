49d2f62ddc09f44a72e0dbdfe144a8a086adb423
JartzDz <julilojano2412@gmail.com>
2023-11-24 09:47:57 -0500

    Nueva Interfaz

--- src/Vista/InterfazMonitoreo.java
+++ src/Vista/InterfazMonitoreo.java
@@ -1,4 +1,12 @@
 package Vista;
 
+import javax.swing.*;
+
 public class InterfazMonitoreo {
+    private JTextField textField2;
+    private JButton AGREGARButton;
+    private JButton BUSCARButton;
+    private JButton MODIFICARButton;
+    private JButton ELIMINARButton;
+    private JComboBox comboBox1;
 }
