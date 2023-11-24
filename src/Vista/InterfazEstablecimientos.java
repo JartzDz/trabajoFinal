6a6bf7213be0bfebe3fdadfe7c6f96bfb8b0d4d2
JartzDz <julilojano2412@gmail.com>
2023-11-23 21:16:16 -0500

    interfazv2

--- src/Vista/InterfazEstablecimientos.java
+++ src/Vista/InterfazEstablecimientos.java
@@ -2,21 +2,19 @@ package Vista;
 
 import javax.swing.*;
 
-public class InterfazEstablecimiento {
-    public JPanel panel1;
-    public JPanel JPanelFondo;
-    public JTable tablaMascotas;
+public class InterfazEstablecimientos extends JFrame{
+    public JTable tablaEstablecimientos;
     public JTextField txtBuscar;
     public JButton btnBuscar;
     public JTextField txtID;
     public JTextField txtNombre;
     public JTextField txtRaza;
     public JTextField txtDuenio;
-    public JTextField textField1;
+    public JTextField txtCorreo;
+    public JComboBox cboTipoEst;
+    public JTextField txtCIProp;
     public JButton btnAgregar;
     public JButton btnMostrarMascotas;
     public JButton btnEliminar;
     public JButton btnModificar;
-    private JComboBox comboBox1;
-    private JTextField textField2;
 }
