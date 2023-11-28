572dbc88b38f572bedadad7cb581f72b8e316159
JartzDz <julilojano2412@gmail.com>
2023-11-27 19:12:24 -0500

    Nuevas interfaces

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,7 @@ public class Main {
         InterfazMascotas interfazMascotas = new InterfazMascotas();
         GestorMascotas gestorMascotas = new GestorMascotas();
         ControladorMascotas controladorMascotas = new ControladorMascotas(gestorMascotas, interfazMascotas);
-        String cedula = "1234567890"; // Reemplaza con la cédula deseada
-        String nombre = "juli"; // Reemplaza con el nombre deseado
-        String direccion = "Dirección Admin"; // Reemplaza con la dirección deseada
-        String telefono = "123456789"; // Reemplaza con el teléfono deseado
-        String correo = "admin@example.com"; // Reemplaza con el correo deseado
-        String contrasenia = "admin123"; // Reemplaza con la contraseña deseada
-        String tipoUsuario = "ADMINISTRADOR";
-
-
+        
 
         GestorUsuario gestorUsuario = new GestorUsuario();
         InterfazUsuarios interfazUsuarios = new InterfazUsuarios();
